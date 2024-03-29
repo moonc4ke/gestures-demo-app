04e81e609f8b2a940f9553730b6a504fb19a3c35
mooncake <adom.donatas@gmail.com>
2024-03-29 17:52:56 +0800

    Update gestures

--- gestures.js
+++ gestures.js
@@ -1,4 +1,4 @@
 import GesturesSDK from '@adom.donatas/gestures-sdk';
 
-const gesturesSDK = new GesturesSDK('https://fake-node-server.onrender.com');
+const gesturesSDK = new GesturesSDK('https://fake-node-server.onrender.com/events');
 gesturesSDK.init();
